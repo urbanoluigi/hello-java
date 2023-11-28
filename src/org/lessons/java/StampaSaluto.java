9ec411a08dc1d4b769fd223178715f0b4b125182
urbanoluigi <urrbanoluigi@gmail.com>
2023-11-28 21:23:36 +0100

    finish

--- src/org/lessons/java/StampaSaluto.java
+++ src/org/lessons/java/StampaSaluto.java
@@ -1,2 +1,8 @@
-package org.lessons.java;public class StampaSaluto {
+package org.lessons.java;
+
+public class StampaSaluto {
+    public static void main(String[] args) {
+        //qui codice eseguibile
+        System.out.println("Hello World!!!");
+    }
 }
